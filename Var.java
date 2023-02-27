291ef46398830b85d1688a6adbcd201112a26b9f
lksAnton <lukusaanton@gmail.com>
2023-02-27 13:38:34 +0100

    java 03

---
+++ Var.java
@@ -0,0 +1,14 @@
+class Var {
+    public static void main(String[] args) {
+        String name = "Indiana Jones";
+        Boolean isWatched = false;
+        int releaseDate = 1989;
+        float imdbNote = 8.2f;
+
+        System.out.println(name);
+        System.out.println(isWatched);
+        System.out.println(releaseDate);
+        System.out.println(imdbNote);
+
+    }
+}
